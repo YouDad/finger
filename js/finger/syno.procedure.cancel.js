44bfa5678d3e15790467e5ace26f72c88439f5bf
manjaro-pc <1632083718@qq.com>
2019-11-18 22:42:09 +0800

    feature: cancel output log

--- js/finger/syno.procedure.cancel.js
+++ js/finger/syno.procedure.cancel.js
@@ -1,6 +1,10 @@
 {
     let procedures = {
         "begin": async function () {
+            $user_log("取消操作", "warning");
+            $procedure.next("loop");
+        },
+        "loop": async function () {
             
         },
     };
